844ee0410d2e6c1545307be3101b5ce6902cb67b
Andrew O'Shei <andrewoshei@gmail.com>
2022-11-24 03:36:53 +0100

    first

---
+++ main.py
@@ -0,0 +1,75 @@
+#!/usr/bin/env python3
+
+import sys
+from flask import Flask, request
+from operations.WalletOps import create_wallet, check_funding
+
+app = Flask(__name__)
+
+''' Handle New User POST requests '''
+@app.route("/create_user", methods=['POST'])
+def create_user():
+    content_type = request.headers.get('Content-Type')
+    if (content_type == 'application/json'):
+        json = request.json
+        # TODO check if username already exists
+        # If not generate a wallet
+        wallet_address, private_key = create_wallet()
+        print("Processing new user: {}".format(json))
+        return "ok\n"
+    else:
+        # Handle unsupported content types
+        return 'Content-type not supported.'
+
+
+''' Handle Transaction POST requests '''
+@app.route("/transaction", methods=['POST'])
+def transaction():
+    content_type = request.headers.get('Content-Type')
+    if (content_type == 'application/json'):
+        json = request.json
+        print("Processing transaction: {}".format(json))
+        return "ok\n"
+    else:
+        # Handle unsupported content types
+        return 'Content-type not supported.'
+
+
+''' Handle Smart-Contract create POST requests '''
+@app.route("/write_contract", methods=['POST'])
+def write_contract():
+    content_type = request.headers.get('Content-Type')
+    if (content_type == 'application/json'):
+        json = request.json
+        print("Writing contract: {}".format(json))
+        return "ok\n"
+    else:
+        # Handle unsupported content types
+        return 'Content-type not supported.'
+
+
+''' Handle Smart-Contract signer POST requests '''
+@app.route("/sign_contract", methods=['POST'])
+def sign_contract():
+    content_type = request.headers.get('Content-Type')
+    if (content_type == 'application/json'):
+        json = request.json
+        print("Signing contract: {}".format(json))
+        return "ok\n"
+    else:
+        # Handle unsupported content types
+        return 'Content-type not supported.'
+
+
+if __name__ == "__main__":
+    arg = sys.argv[1].lower()
+    if arg == "local":
+        print("Starting local server@127.0.0.1:5000...")
+        app.run(host='127.0.0.1', port=5000)
+    elif arg == "prod":
+        print("Starting production server@0.0.0.0:80...")
+        app.run(host='0.0.0.0', port=80)
+    else:
+        print('    Missing argument:\n' + 
+              '\tlocal - for running tests\n' + 
+              '\tprod  - to put server online')
---
+++ operations/WalletOps.py
@@ -0,0 +1,30 @@
+#!/usr/bin/env python3
+
+from algosdk import account, mnemonic
+from algosdk.v2client import algod
+
+def get_algod_client():
+    algod_token = "aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa"
+    algod_address = "http://localhost:4001"
+    algod_client = algod.AlgodClient(algod_token, algod_address)
+    return algod_client
+
+def create_wallet():
+    secret_key, my_address = account.generate_account()
+    m = mnemonic.from_private_key(secret_key)
+    print("My address: {}".format(my_address))
+    print("My private key: {}".format(secret_key))
+    print("My passphrase: {}".format(m))
+    return (my_address, secret_key)
+
+def check_funding(wallet_address):
+    client = get_algod_client()
+    account_info = algod_client.account_info(wallet_address)
+    amount = account_info.get('amount')
+    # Returns a dictionary of assets
+    assets = account_info.get('assets')
+    for asa in assets:
+        if asa['asset-id'] == 14512352:
+            token_amount = int(asa['amount'])
+    print("Account balance: {} microAlgos".format(account_info.get('amount')) + "\n")
+    return int(amount)
